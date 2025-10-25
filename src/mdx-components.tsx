c19c18de77ee44f456f93ccd1a4e7906caa424c2
Pavel Eliseev <forgenet@inbox.ru>
2025-10-25 22:57:32 +0300

    Add code with output mdx component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { CodeExample } from '@/components/code-example';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    CodeExample,
     ...components,
   };
 }
