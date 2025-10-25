38ffc9cadabf960ab2209a435682ba465155483c
Pavel Eliseev <forgenet@inbox.ru>
2025-10-25 22:57:32 +0300

    Add code with output mdx component

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs"
+import * as docs_1 from "../content/docs/index.mdx?collection=docs"
+import * as docs_0 from "../content/docs/guide.mdx?collection=docs"
 import { _runtime } from "fumadocs-mdx/runtime/next"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","fullPath":"content/docs/index.mdx"}, data: docs_0 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"guide.mdx","fullPath":"content/docs/guide.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","fullPath":"content/docs/index.mdx"}, data: docs_1 }], [])
