ffa178ca3282e70feafe0aabd591edc3f21f9128
Pavel Eliseev <forgenet@inbox.ru>
2025-11-14 13:42:34 +0300

    Add gost quote component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,14 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+
 import { CodeExample } from '@/components/code-example';
+import { GostQuote } from './components/docs/quote';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     CodeExample,
+    GostQuote,
     ...components,
   };
 }
