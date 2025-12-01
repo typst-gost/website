eb11c430f6714f7c14c17c9ea0cb4d6f561ffce2
Pavel Eliseev <forgenet@inbox.ru>
2025-12-01 11:11:14 +0300

    Fix yandex metrics

--- src/components/yandex-metrics.tsx
+++ src/components/yandex-metrics.tsx
@@ -11,38 +11,27 @@ export function YandexMetrika() {
   useEffect(() => {
     const url = `${pathname}?${searchParams}`;
     if (typeof window !== 'undefined' && typeof window.ym !== 'undefined') {
-        window.ym(105476873, 'hit', url);
+      window.ym(105476873, 'hit', url);
     }
   }, [pathname, searchParams]);
 
   return (
-    <>
-      <Script id="yandex-metrika" strategy="afterInteractive">
-        {`
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    <Script id="yandex-metrika" strategy="afterInteractive">
+      {`
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-          ym(105476873, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true,
-            ecommerce:"dataLayer"
-          });
-        `}
-      </Script>
-      <noscript>
-        <div>
-          <img 
-            src="https://mc.yandex.ru/watch/105476873" 
-            style={{ position: 'absolute', left: '-9999px' }} 
-            alt="" 
-          />
-        </div>
-      </noscript>
-    </>
+        ym(105476873, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true,
+          ecommerce:"dataLayer"
+        });
+      `}
+    </Script>
   );
 }
