5f2aa3c0a3ad5910c9a6bf93b169c703d6bd1209
Pavel Eliseev <forgenet@inbox.ru>
2026-01-14 21:37:20 +0300

    Fix relative links

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,6 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, ElementType } from 'react';
 
 import { TypstRender } from './components/docs/typst/renderer';
 import { GostQuote } from './components/docs/quote';
@@ -8,6 +8,7 @@ import { QuoteLink } from './components/docs/quote-link';
 import { FurtherReading } from './components/docs/further-reading';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
+  const RelativeLink = components?.a as ElementType;
   return {
     ...defaultMdxComponents,
     TypstRender,
@@ -18,6 +19,9 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     a: (props: ComponentPropsWithoutRef<'a'>) => {
       if (props.href?.startsWith('#')) {
         return <QuoteLink {...props} href={props.href} />;
+      }
+      if (RelativeLink) {
+        return <RelativeLink {...props} />;
       }
       return <a {...props} target="_blank" rel="noopener noreferrer" />;
     },
